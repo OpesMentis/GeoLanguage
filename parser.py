941e2e2b2caa912a6803ab49c5dbe1ee5fa6f2a9
Antoine Planchot <antoine.planchot@ensta-bretagne.org>
2016-05-14 00:22:18 +0200

    Début de réécriture du parser avec une grammaire plus claire.

--- parser.py
+++ parser.py
@@ -18,7 +18,7 @@ class Parser:
 
 		print("+++ PARSER +++")
 		while True:
-			self.parseInst()
+			self.parseInstruction()
 
 	def expect(self, token_kind):
 		next_tok = self.lexer.getNext()
@@ -35,69 +35,185 @@ class Parser:
 		token = self.lexer.getNext()
 		return token
 
-	def parseInstPoint(self):
-		print ("Parse instanciation point")
+	def parseInstruction(self):
+		print("Parse instruction")
+		if self.showNext().kind in ["pnt", "vect", "line", "seg", "ang", "scal", "string"]:
+			self.parseDeclaration()
+		#elif self.showNext().kind == 'ident':
+			#self.parseAffectation()
+		else:
+			print ("GOSH! Parsing error for Instruction ligne : ", self.showNext().pos)
+			sys.exit(0)
+
+	def parseDeclaration(self):
+		print ("Parse declaration")
+		if self.showNext().kind == 'pnt':
+			self.parseDeclPnt()
+		elif self.showNext().kind == 'line':
+			self.parseDeclLine()
+		elif self.showNext().kind == 'vect':
+			self.parseDeclVect()
+		elif self.showNext().kind == 'seg':
+			self.parseDeclSeg()
+		elif self.showNext().kind == 'scal':
+			self.parseDeclScal()
+		elif self.showNext().kind == 'string':
+			self.parseDeclString()
+		else:
+			print ("GOSH! Parsing error for Declaration ligne : ", self.showNext().pos)
+			sys.exit(0)
+
+	def parseDeclPnt(self):
+		print ("Parse declaration point")
+		self.expect('pnt')
+		self.expect('ident')
+		if self.showNext().kind != 'excl':
+			self.expect('colon')
+			self.parseObjPnt()
+		self.expect('excl')
+
+	def parseDeclLine(self):
+		print ("Parse declaration line")
+		self.expect('line')
+		self.expect('ident')
+		if self.showNext().kind != 'excl':
+			self.expect('colon')
+			self.parseObjLine()
+		self.expect('excl')
+
+	def parseDeclVect(self):
+		print ("Parse declaration vector")
+		self.expect('vect')
+		self.expect('ident')
+		if self.showNext().kind != 'excl':
+			self.expect('colon')
+			self.parseObjVect()
+		self.expect('excl')
+
+	def parseDeclSeg(self):
+		print ("Parse declaration segment")
+		self.expect('seg')
+		self.expect('ident')
+		if self.showNext().kind != 'excl':
+			self.expect('colon')
+			self.parseObjSeg()
+		self.expect('excl')
+
+	def parseDeclScal(self):
+		print ("Parse declaration segment")
+		self.expect('scal')
+		self.expect('ident')
+		if self.showNext().kind != 'excl':
+			self.expect('colon')
+			self.parseObjScal()
+		self.expect('excl')
+
+	def parseDeclString(self):
+		print ('Parse declaration string')
+		self.expect('string')
+		self.expect('ident')
+		if self.showNext().kind != 'excl':
+			self.expect('colon')
+			self.parseObjString()
+		self.expect('excl')
+
+	def parseInstPnt(self):
+		print ('Parse instanciation point')
 		self.expect('pnt')
 		self.expect('l_par')
-		self.expect('scal')
+		self.parseObjScal()
 		self.expect('comma')
-		self.expect('scal')
+		self.parseObjScal()
 		self.expect('r_par')
 
 	def parseInstLine(self):
-		print ("Parse instanciation line")
+		print ('Parse instanciation ligne')
 		self.expect('line')
 		self.expect('l_par')
-		if self.showNext().kind == 'ident':
-			acceptIt()
-		elif self.showNext().kind == 'pnt':
-			parseInstPoint()
-		else:
-			 raise ("Parsing error for InstLine ligne : ", self.showNext().pos)
+		self.parseObjPnt()
 		self.expect('comma')
-		if self.showNext().kind == 'ident':
-			acceptIt()
-		elif self.showNext().kind == 'pnt':
-			self.parseInstPoint()
-		elif self.showNext().kind == 'vect':
-			self.parseInstVector()
-		else:
-			 raise ("Parsing error for InstLine ligne : ", self.showNext().pos)
-		self.expect('r_par')
+		self.parseObjPnt()
 
-	def parseInstVector(self):
-		print ("Parse instanciation vector")
+	def parseInstVect(self):
+		print ('Parse instanciation vector')
 		self.expect('vect')
 		self.expect('l_par')
-		self.expect('scal')
+		self.parseObjScal()
 		self.expect('comma')
-		self.expect('scal')
+		self.parseObjScal()
 		self.expect('r_par')
 
-	def parseString(self):
-		print ("Parse string")
-		self.expect('string')
+	def parseInstSeg(self):
+		print ('Parse instanciation segment')
+		self.expect('seg')
+		self.expect('l_par')
+		self.parseObjPnt()
+		self.expect('comma')
+		self.parseObjPnt()
+		self.expect('r_par')
 
-	def parseScal(self):
-		print ("Parse scalar")
-		self.expect('scal')
-
-	def parseInst(self):
-		print ("Parse instanciation")
-		self.expect('ident')
-		self.expect('colon')
+	def parseObjPnt(self):
+		print ('Parse objet point')
 		if self.showNext().kind == 'pnt':
-			self.parseInstPoint()
-		elif self.showNext().kind == 'line':
-			self.parseInstLine()
-		elif self.showNext().kind == 'vect':
-			self.parseInstVector()
-		elif self.showNext().kind == 'quote':
-			self.parseString()
-		elif self.showNext().kind == 'scal':
-			self.parseScal()
+			self.parseInstPnt()
+		elif self.showNext().kind == 'ident':
+			self.expect('ident')
 		else:
-			raise ("Parsing error for Inst ligne : ", self.showNext().pos)
+			print ("GOSH! Parsing error for ObjPoint ligne : ", self.showNext().pos)
+			sys.exit(0)
+
+	def parseObjLine(self):
+		print ('Parse objet line')
+		if self.showNext().kind == 'line':
+			self.parseInstLine()
+		elif self.showNext().kind == 'ident':
+			self.expect('ident')
+		else:
+			print ("GOSH! Parsing error for ObjLine ligne : ", self.showNext().pos)
+			sys.exit(0)
+
+	def parseObjVect(self):
+		print ('Parse objet vector')
+		if self.showNext().kind == 'vect':
+			self.parseInstVect()
+		elif self.showNext().kind == 'ident':
+			self.expect('ident')
+		else:
+			print ("GOSH! Parsing error for ObjVect ligne : ", self.showNext().pos)
+			sys.exit(0)
+
+	def parseObjSeg(self):
+		print ('Parse objet segment')
+		if self.showNext().kind == 'seg':
+			self.parseInstSeg()
+		elif self.showNext().kind == 'ident':
+			self.expect('ident')
+		else:
+			print ("GOSH! Parsing error for ObjSeg ligne : ", self.showNext().pos)
+			sys.exit(0)
+
+	def parseObjScal(self):
+		print('Parse objet scalaire')
+		if self.showNext().kind == 'minus':
+			self.expect('minus')
+			self.expect('nb')
+		elif self.showNext().kind == 'nb':
+			self.expect('nb')
+		elif self.showNext().kind == 'ident':
+			self.expect('ident')
+		else:
+			print ("GOSH! Parsing error for ObjScal ligne : ", self.showNext().pos)
+			sys.exit(0)
+
+	def parseObjString(self):
+		print('Parse objet string')
+		if self.showNext().kind == 'ch':
+			self.expect('ch')
+		elif self.showNext().kind == 'ident':
+			self.expect('ident')
+		else:
+			print ("GOSH! Parsing error for ObjScal ligne : ", self.showNext().pos)
+			sys.exit(0)
 
 if __name__ == '__main__':
     parser = Parser()
